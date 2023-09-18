05225aaee261be5548bfeec184d291add6f152d3
pdcxs <pdcxs@outlook.com>
2023-09-18 19:37:34 +0800

    Add test to prob 337

--- src/337-house-robber-iii.rkt
+++ src/337-house-robber-iii.rkt
@@ -6,25 +6,27 @@
 (struct tree-node
   (val left right) #:mutable #:transparent)
 
-; constructor
-(define (make-tree-node [val 0])
-  (tree-node val #f #f))
-
 ; robbing a hourse
 ; is the maximum of
 ; rob this hourse?
 ; or not.
 (define (rob root)
-  (if (not root) 0
-      (let ([robbed
-             (+ (tree-node-val root)
+  (if (not root) 0 ; for a null root, cannot rob anymore
+      (let ([robbed ; how much can we rob when rob root node
+             (+ (tree-node-val root) ; rob root
+                ; rob children of left child
                 (rob-child (tree-node-left root))
+                ; rob children of right child
                 (rob-child (tree-node-right root)))]
+            ; how much can we rob when don't rob root node
+            ; rob left child and rob right child
             [not-rob (+ (mem-rob (tree-node-left root))
                         (mem-rob (tree-node-right root)))])
+        ; take the maximum of robbing and not robbing root
         (max robbed not-rob))))
 
-(define (mem-f f)
+; memoization
+(define (mem f)
   (define h (make-hash))
   (λ (x)
     (if (hash-has-key? h x)
@@ -33,11 +35,30 @@
           (hash-set! h x v)
           v))))
 
-(define mem-rob (mem-f rob))
+(define mem-rob (mem rob))
 
+; Robs the left tree and right tree of root.
+; Don't rob the root itself.
 (define (rob-child root)
   (if (not root) 0
       (+ (mem-rob (tree-node-left root))
          (mem-rob (tree-node-right root)))))
 
-; =========
+; =========== Test ===============
+(require rackunit)
+(check-equal?
+ (rob (tree-node
+       3
+       (tree-node 2 #f (tree-node 3 #f #f))
+       (tree-node 3 #f (tree-node 1 #f #f))))
+ 7)
+
+(check-equal?
+ (rob (tree-node
+       3
+       (tree-node
+        4 (tree-node 1 #f #f)
+        (tree-node 3 #f #f))
+       (tree-node 5 #f
+                  (tree-node 1 #f #f))))
+ 9)
