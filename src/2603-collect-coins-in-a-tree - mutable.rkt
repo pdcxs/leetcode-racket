12b62d6e6162a981ef94f26d9008d73c9451eec7
pdcxs <pdcxs@outlook.com>
2023-09-22 18:22:51 +0800

    Solve prob 2603

--- src/2603-collect-coins-in-a-tree - mutable.rkt
+++ src/2603-collect-coins-in-a-tree - mutable.rkt
@@ -1,117 +1,89 @@
 #lang racket
 
 ; A graph is needed.
-; A graph is a hash-table
-; key is node
-; value is its neighbors
-; The hash-table based method is too slow,
-; so I tried vector, but it exceeds
-; memory limitation. So I use mutable
-; hash-table here. But this is even worse.
-; I give up, optimizing the performance
-; of functional program is too hard for me.
+; A graph is a vector
+; graph[i] is a set of nodes which are
+; neighbors of node i.
+; Note: this solution is very ugly,
+; but it works.
 
-(define (build-graph n edges)
-  (define graph (make-hash))
-  (define degree (make-vector n))
-  (define (scan edges)
-    (unless (null? edges)
-      (let* ([n1 (first (first edges))]
-             [n2 (second (first edges))]
-             [nb1 (vector-append (vector n2) (hash-ref graph n1 (vector)))]
-             [nb2 (vector-append (vector n1) (hash-ref graph n2 (vector)))])
-        (begin
-          (hash-set! graph n1 nb1)
-          (hash-set! graph n2 nb2)
-          (vec-add1! degree n1)
-          (vec-add1! degree n2)
-          (scan (rest edges))))))
-  (scan edges) (cons graph degree))
-
-(define (vec-add1! vec index)
+(define (vector-add1! vec index)
   (vector-set!
    vec index
    (add1 (vector-ref vec index))))
 
-(define (vec-sub1! vec index)
+(define (vector-sub1! vec index)
   (vector-set!
    vec index
    (sub1 (vector-ref vec index))))
 
-; remove leaf nodes with zero coin
-(define (remove-zero-leaf! graph degree coins)
-  (define need-removed '())
+(define (collect-the-coins cs edges)
+  (define coins (list->vector cs))
   (define n (vector-length coins))
-  (define (get-removed-nodes!)
-    (for ([i (range n)])
-      (when
-          (and
-           (= (vector-ref degree i) 1)
-           (= (vector-ref coins i) 0))
-        (set! need-removed (cons i need-removed))
-        (vec-sub1! degree i)
-        (for ([j (hash-ref graph i '())])
-          (vec-sub1! degree j)))))
 
-  (get-removed-nodes!)
-  (unless (null? need-removed)
-    (remove-nodes! graph need-removed)
-    (remove-zero-leaf! graph degree coins))
-  graph)
+  ; initial graph and degree vector
+  (define graph (build-vector n (λ (i) (mutable-set))))
+  (define degree (make-vector n))
 
-; remove a node in a graph
-(define (remove-node! graph node)
-  (hash-remove! graph node)
-  (let loop ([i (hash-iterate-first graph)])
-    (if i
-        (begin
-          (hash-update!
-           graph (hash-iterate-key graph i)
-           (λ (nb) (vector-filter
-                    (λ (n) (not (= n node)))
-                    nb)))
-          (loop
-           (hash-iterate-next graph i)))
-        graph)))
+  ; Build graph
+  (for ([e edges])
+    (let ([n1 (first e)]
+          [n2 (second e)])
+      (set-add!
+       (vector-ref graph n1) n2)
+      (set-add!
+       (vector-ref graph n2) n1)
+      (vector-add1! degree n1)
+      (vector-add1! degree n2)))
 
-; remove a list of nodes
-(define (remove-nodes! graph nodes)
-  (if (null? nodes)
-      graph
-      (begin
-        (remove-node! graph (first nodes))
-        (remove-nodes!
-         graph
-         (rest nodes)))))
+  (define queue (make-vector n))
+  (define queue-head 0)
+  (define queue-tail 0)
+  (define rest n)
 
-; delete all leaf nodes
-(define (delete-leaf! graph degree)
-  (define leaves
-    (filter
-     (λ (i) (= 1 (vector-ref degree i)))
-     (range (vector-length degree))))
-  (for ([leaf leaves])
-    (begin
-      (vec-sub1! degree leaf)
-      (let ([father
-             (vector-ref
-              (hash-ref graph leaf) 0)])
-        (vec-sub1! degree father))))
-  (remove-nodes! graph leaves))
+  ; delete all leaves without coin
+  (for ([i (in-range n)])
+    (when (and
+           (is-leaf degree i)
+           (zero? (vector-ref coins i)))
+      (vector-set! queue queue-tail i)
+      (set! queue-tail (add1 queue-tail))))
 
-; Main function
-(define (collect-the-coins coins edges)
-  (define cs (list->vector coins))
-  (define n (vector-length cs))
-  (define gd (build-graph n edges))
-  (define graph (car gd))
-  (define degree (cdr gd))
-  (remove-zero-leaf! graph degree cs)
-  (delete-leaf! graph degree)
-  (delete-leaf! graph degree)
-  (let ([node-num (hash-count graph)])
-    (if (zero? node-num) 0
-        (- (* 2 node-num) 2))))
+  (for ([i (in-naturals)]
+        #:break (= queue-head queue-tail))
+    (let ([u (vector-ref queue queue-head)])
+      (set! queue-head (add1 queue-head))
+      (vector-sub1! degree u)
+      (set! rest (sub1 rest))
+      (for ([v (vector-ref graph u)])
+        (vector-sub1! degree v)
+        (when
+            (and (is-leaf degree v)
+                 (zero? (vector-ref coins v)))
+          (vector-set! queue queue-tail v)
+          (set! queue-tail (add1 queue-tail))))))
+  
+  ; delete twice leaves
+  (for ([j (in-range 2)])
+    (set! queue-head 0)
+    (set! queue-tail 0)
+    (for ([i (in-range n)])
+      (when (is-leaf degree i)
+        (vector-set! queue queue-tail i)
+        (set! queue-tail (add1 queue-tail))))
+    (for ([i (in-naturals)]
+          #:break (= queue-head queue-tail))
+      (let ([u (vector-ref queue queue-head)])
+        (set! queue-head (add1 queue-head))
+        (vector-sub1! degree u)
+        (set! rest (sub1 rest))
+        (for ([v (vector-ref graph u)])
+          (vector-sub1! degree v)))))
+  (if (= 0 rest) 0
+      (* 2 (sub1 rest))))
+
+(define (is-leaf degree i)
+  (= 1 (vector-ref degree i)))
 
 ; ============= test =====================
 (require rackunit)
