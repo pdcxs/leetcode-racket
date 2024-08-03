c098787c137e95bbc2b767cbf3d3d31026e17e8f
Eric Brown <pdcxs@outlook.com>
2024-08-03 18:00:30 +0800

    Add UnitTest to 2766-relocate-marbles.rkt

--- src/2766-relocate-marbles.rkt
+++ src/2766-relocate-marbles.rkt
@@ -8,3 +8,14 @@
                [ts (set-add fs (first to))])
           (scan (rest from) (rest to) ts))))
   (sort (scan from to locs) <))
+
+
+; ================== Test ==========================
+(require rackunit)
+
+(check-equal?
+ (relocate-marbles '(1 6 7 8) '(1 7 2) '(2 9 5))
+ '(5 6 8 9))
+(check-equal?
+ (relocate-marbles '(1 1 3 3) '(1 3) '(2 2))
+ '(2))
