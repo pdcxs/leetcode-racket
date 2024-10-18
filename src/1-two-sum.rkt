3e77f1fea6a10eaf22617b83224249c18e9c041f
Eric Brown <pdcxs@outlook.com>
2024-10-18 11:12:32 +0800

    Update 1-two-sum.rkt
    Use default parameter, make the solution even faster.

--- src/1-two-sum.rkt
+++ src/1-two-sum.rkt
@@ -1,12 +1,12 @@
 #lang racket
 
-(define (two-sum nums target)
-  (let iter ([l nums] [h (hash)] [i 0])
-    (if (hash-has-key? h (car l))
-        (list (hash-ref h (car l)) i)
-        (iter (cdr l)
-              (hash-set h (- target (car l)) i)
-              (add1 i)))))
+(define (two-sum nums target [index 0] [mem (hash)])
+  (let* ([n (first nums)]
+         [other-num (- target n)])
+    (if (hash-has-key? mem other-num)
+        (list (hash-ref mem other-num) index)
+        (two-sum (rest nums) target (add1 index)
+                 (hash-set mem n index)))))
 
 ;=========== Test =============
 (require rackunit)
@@ -14,9 +14,11 @@
 (check-equal?
  (two-sum '(2 7 11 15) 9)
  '(0 1))
+
 (check-equal?
  (two-sum '(3 2 4) 6)
  '(1 2))
+
 (check-equal?
  (two-sum '(3 3) 6)
  '(0 1))
